518a62feac75e7c3809f264815476f6b6771aedf
kevinhooke <kevin.hooke@gmail.com>
2020-08-09 13:02:31 -0700

    added puzzle valid check

--- src/main/java/kh/sudoku/SudokuGenerator.java
+++ src/main/java/kh/sudoku/SudokuGenerator.java
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Sudoku puzzle generator. Uses kh.sudoku.SudokuSolverWithDLX to check for valid puzzles
- * and kh.sudokugrader.SudokuGraderApp to check puzzle difficult.
+ * Sudoku puzzle generator. Uses kh.sudoku.SudokuSolverWithDLX to check for
+ * valid puzzles and kh.sudokugrader.SudokuGraderApp to check puzzle difficulty.
  * 
  * @author kevinhooke
  *
@@ -18,21 +18,21 @@ public class SudokuGenerator {
     public String generateRandomSeedString() {
         List<String> startingList = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
         Collections.shuffle(startingList);
-        
+
         StringBuilder sb = new StringBuilder();
-        for(String nextNumber : startingList) {
+        for (String nextNumber : startingList) {
             sb.append(nextNumber);
         }
-        
+
         return sb.toString();
     }
-    
-    public PuzzleResults generate() {
-        
+
+    public PuzzleResults generate(int candidatesToRemove) {
+
         SudokuSolverWithDLX solver = new SudokuSolverWithDLX();
-        //create seed for starting final solution
+        // create seed for starting final solution
         List<String> givenSolutionsShorthand = new ArrayList<>();
-        givenSolutionsShorthand.add("735614928");
+        givenSolutionsShorthand.add(this.generateRandomSeedString());
         givenSolutionsShorthand.add(".........");
         givenSolutionsShorthand.add(".........");
         givenSolutionsShorthand.add(".........");
@@ -42,39 +42,35 @@ public class SudokuGenerator {
         givenSolutionsShorthand.add(".........");
         givenSolutionsShorthand.add(".........");
         PuzzleResults results = solver.generateNewPuzzle(givenSolutionsShorthand, 1);
-        //assertFalse(results.isValidPuzzle());
-        
-        // int givens to remove (up to 64, leaving min of 17)
+
+        // initial givens to remove (up to 64, leaving min of 17)
         boolean stillValid = true;
         boolean continueRemovingCells = true;
         int candidatesRemoved = 0;
-        int candidatesToRemove = 10;
+        PuzzleResults results2 = null;
         List<String> result1 = results.getResults().get(0);
-        
-        while(stillValid && continueRemovingCells) {
+
+        while (stillValid && continueRemovingCells && candidatesRemoved < candidatesToRemove) {
             int row = new Random().nextInt(9);
             int col = new Random().nextInt(9);
             // is there a number still in this post?
-             if(this.removeCandidateFromPosition(row, col, result1)) {
-                 if(candidatesToRemove == candidatesToRemove) {
-                     continueRemovingCells = false;
-                 }
-             }
+            boolean cellRemoved = this.removeCandidateFromPosition(row, col, result1);
+            candidatesRemoved++;
+            // check the puzzle is valid, if still valid, continue
+            solver = new SudokuSolverWithDLX();
+            results2 = solver.run(result1, 1);
+            stillValid = results2.isValidPuzzle();
             
-            //check the puzzle is value
-            //if still valid, continue
-        
-            //replace 1 candidate, pass back to solver
+            // replace 1 candidate, pass back to solver
             result1 = results.getResults().get(0);
-    //        String row1 = result1.get(0);
-    //        row1 = row1.substring(1);
-    //        row1 = "." + row1;
-    //        result1.set(0, row1);
-    //        results.getResults().set(0, result1);
+            // String row1 = result1.get(0);
+            // row1 = row1.substring(1);
+            // row1 = "." + row1;
+            // result1.set(0, row1);
+            // results.getResults().set(0, result1);
         }
-        
-        solver = new SudokuSolverWithDLX();
-        PuzzleResults results2 = solver.run(result1, 1);
+
+        //TODO: if not valid, need to backtrack or start again
         
         return results2;
     }
@@ -83,5 +79,5 @@ public class SudokuGenerator {
         // TODO Auto-generated method stub
         return false;
     }
-    
+
 }
--- src/test/java/kh/sudoku/SudokuGeneratorTest.java
+++ src/test/java/kh/sudoku/SudokuGeneratorTest.java
@@ -20,36 +20,6 @@ import org.junit.Test;
  */
 public class SudokuGeneratorTest {
     
-    private String easySolution = 
-            "+-------+-------+-------+\n" + 
-            "| 3 4 9 | 8 1 2 | 6 7 5 | \n" + 
-            "| 5 1 7 | 4 9 6 | 2 3 8 | \n" + 
-            "| 2 6 8 | 3 5 7 | 1 9 4 | \n" + 
-            "+-------+-------+-------+\n" + 
-            "| 1 8 5 | 7 2 3 | 9 4 6 | \n" + 
-            "| 4 9 3 | 6 8 1 | 5 2 7 | \n" + 
-            "| 7 2 6 | 9 4 5 | 8 1 3 | \n" + 
-            "+-------+-------+-------+\n" + 
-            "| 9 7 2 | 5 3 8 | 4 6 1 | \n" + 
-            "| 6 5 1 | 2 7 4 | 3 8 9 | \n" + 
-            "| 8 3 4 | 1 6 9 | 7 5 2 | \n" + 
-            "+-------+-------+-------+\n";
-    
-    private static final List<String> easySolutionShorthand = new ArrayList<>();
-    
-    @BeforeClass
-    public static void init(){
-        easySolutionShorthand.add("349812675");
-        easySolutionShorthand.add("517496238");
-        easySolutionShorthand.add("268357194");
-        easySolutionShorthand.add("185723946");
-        easySolutionShorthand.add("493681527");
-        easySolutionShorthand.add("726945813");
-        easySolutionShorthand.add("972538461");
-        easySolutionShorthand.add("651274389");
-        easySolutionShorthand.add("834169752");
-    }
-    
     @Test
     public void testShuffledStartingList() {
         String startingSeed = "123456789";
@@ -83,19 +53,8 @@ public class SudokuGeneratorTest {
     public void testGenerate1() {
         SudokuSolverWithDLX solver = new SudokuSolverWithDLX();
         SudokuGenerator generator = new SudokuGenerator();
-        String generatedSeed1 = generator.generateRandomSeedString();
         
-        List<String> givenSolutionsShorthand = new ArrayList<>();
-        givenSolutionsShorthand.add(generatedSeed1);
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        givenSolutionsShorthand.add(".........");
-        PuzzleResults results = solver.run(givenSolutionsShorthand, 10);
+        PuzzleResults results = generator.generate(1);
         
         List<List<String>> resultShorthand = results.getResults();
         for(List<String> shorthand : resultShorthand) {
